51a20dbf10e805de4ce090f50e8d86f657323f3c
tiago <tiagotavares34@gmail.com>
2022-09-08 11:46:19 -0300

    3 commit: foi implementado o método input para a pessoa add os dados.

--- src/Entidades/TesteImposto.java
+++ src/Entidades/TesteImposto.java
@@ -1,15 +1,44 @@
 package Entidades;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
 public class TesteImposto {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
-		
-		List<Pessoa> list = new ArrayList <Pessoa>();
-		
+
+		List<Pessoa> list = new ArrayList<Pessoa>();
+
+		System.out.print("Insira o número de pessoas: ");
+		int n = teclado.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Dados da pessoa # " + i);
+			System.out.print("Física ou Jurídica(f/j)?");
+			char type = teclado.next().charAt(0);
+			System.out.print("Nome: ");
+			String nome = teclado.next();
+			System.out.print("Renda anual: ");
+			double rendaAnual = teclado.nextDouble();
+
+			if (type == 'f') {
+				System.out.print("Gastos com saúde: ");
+				double GastoComSaude = teclado.nextDouble();
+				Fisica x = new Fisica(nome, rendaAnual, GastoComSaude);
+
+				list.add(x);
+			} else {
+				System.out.print("Número de funcionários: ");
+				int numeroDeFuncionarios = teclado.nextInt();
+				Juridica y = new Juridica(nome, rendaAnual, numeroDeFuncionarios);
+				
+				list.add(y);
+				
+			}
+
+		}
 		teclado.close();
 
 	}
